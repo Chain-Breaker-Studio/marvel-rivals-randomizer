81500432f2e6c6b0955e9fe662e127227b40a319
Sector-Zero <patri_work@hotmail.com>
2026-09-12 03:30:00 +0200

    Create characters.js

---
+++ characters.js
@@ -0,0 +1,34 @@
+const characters = [
+  {
+    name: "SPIDER-MAN",
+    role: "DUELIST"
+  },
+  {
+    name: "GROOT",
+    role: "VANGUARD"
+  },
+  {
+    name: "IRON MAN",
+    role: "DUELIST"
+  },
+  {
+    name: "MANTIS",
+    role: "STRATEGIST"
+  },
+  {
+    name: "MAGNETO",
+    role: "VANGUARD"
+  },
+  {
+    name: "BLACK PANTHER",
+    role: "DUELIST"
+  },
+  {
+    name: "PSYLOCKE",
+    role: "DUELIST"
+  },
+  {
+    name: "DOCTOR STRANGE",
+    role: "VANGUARD"
+  }
+];
