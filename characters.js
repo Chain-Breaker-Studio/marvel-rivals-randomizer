8bf91695f20fda644576a877280c8fe71d4d739f
Sector-Zero <patri_work@hotmail.com>
2026-09-12 10:03:34 +0200

    Update characters.js

--- characters.js
+++ characters.js
@@ -1,63 +1,287 @@
 const characters = [
 
+  // ==========================================
   // VANGUARDS
-  { name: "ANGELA", role: "VANGUARD" },
-  { name: "CAPTAIN AMERICA", role: "VANGUARD" },
-  { name: "DOCTOR STRANGE", role: "VANGUARD" },
-  { name: "EMMA FROST", role: "VANGUARD" },
-  { name: "GROOT", role: "VANGUARD" },
-  { name: "HULK", role: "VANGUARD" },
-  { name: "MAGNETO", role: "VANGUARD" },
-  { name: "PENI PARKER", role: "VANGUARD" },
-  { name: "ROGUE", role: "VANGUARD" },
-  { name: "THE THING", role: "VANGUARD" },
-  { name: "THOR", role: "VANGUARD" },
-  { name: "VENOM", role: "VANGUARD" },
+  // ==========================================
 
+  {
+    name: "ANGELA",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "HULK",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "CAPTAIN AMERICA",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "DEVIL DINOSAUR",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "DOCTOR STRANGE",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "EMMA FROST",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "GROOT",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "MAGNETO",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "PENI PARKER",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "ROGUE",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "THE HOOD",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "THE THING",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "THOR",
+    role: "VANGUARD"
+  },
+
+  {
+    name: "VENOM",
+    role: "VANGUARD"
+  },
+
+
+  // ==========================================
   // DUELISTS
-  { name: "BLACK PANTHER", role: "DUELIST" },
-  { name: "BLACK WIDOW", role: "DUELIST" },
-  { name: "BLADE", role: "DUELIST" },
-  { name: "CABLE", role: "DUELIST" },
-  { name: "CYCLOPS", role: "DUELIST" },
-  { name: "DAREDEVIL", role: "DUELIST" },
-  { name: "ELSA BLOODSTONE", role: "DUELIST" },
-  { name: "GAMBIT", role: "DUELIST" },
-  { name: "GHOST RIDER", role: "DUELIST" },
-  { name: "HAWKEYE", role: "DUELIST" },
-  { name: "HUMAN TORCH", role: "DUELIST" },
-  { name: "IRON FIST", role: "DUELIST" },
-  { name: "IRON MAN", role: "DUELIST" },
-  { name: "JEFF THE LAND SHARK", role: "DUELIST" },
-  { name: "MAGIK", role: "DUELIST" },
-  { name: "MISTER FANTASTIC", role: "DUELIST" },
-  { name: "MOON KNIGHT", role: "DUELIST" },
-  { name: "NAMOR", role: "DUELIST" },
-  { name: "PHOENIX", role: "DUELIST" },
-  { name: "PSYLOCKE", role: "DUELIST" },
-  { name: "SCARLET WITCH", role: "DUELIST" },
-  { name: "SPIDER-MAN", role: "DUELIST" },
-  { name: "STAR-LORD", role: "DUELIST" },
-  { name: "STORM", role: "DUELIST" },
-  { name: "THE HOOD", role: "DUELIST" },
-  { name: "WOLVERINE", role: "DUELIST" },
+  // ==========================================
 
+  {
+    name: "BLACK CAT",
+    role: "DUELIST"
+  },
+
+  {
+    name: "BLACK PANTHER",
+    role: "DUELIST"
+  },
+
+  {
+    name: "BLACK WIDOW",
+    role: "DUELIST"
+  },
+
+  {
+    name: "BLADE",
+    role: "DUELIST"
+  },
+
+  {
+    name: "CYCLOPS",
+    role: "DUELIST"
+  },
+
+  {
+    name: "DAREDEVIL",
+    role: "DUELIST"
+  },
+
+  {
+    name: "DEADPOOL",
+    role: "VANGUARD / DUELIST / STRATEGIST"
+  },
+
+  {
+    name: "ELSA BLOODSTONE",
+    role: "DUELIST"
+  },
+
+  {
+    name: "GORR",
+    role: "DUELIST"
+  },
+
+  {
+    name: "HAWKEYE",
+    role: "DUELIST"
+  },
+
+  {
+    name: "HELA",
+    role: "DUELIST"
+  },
+
+  {
+    name: "HUMAN TORCH",
+    role: "DUELIST"
+  },
+
+  {
+    name: "IRON FIST",
+    role: "DUELIST"
+  },
+
+  {
+    name: "IRON MAN",
+    role: "DUELIST"
+  },
+
+  {
+    name: "MAGIK",
+    role: "DUELIST"
+  },
+
+  {
+    name: "MISTER FANTASTIC",
+    role: "DUELIST"
+  },
+
+  {
+    name: "MOON KNIGHT",
+    role: "DUELIST"
+  },
+
+  {
+    name: "NAMOR",
+    role: "DUELIST"
+  },
+
+  {
+    name: "PHOENIX",
+    role: "DUELIST"
+  },
+
+  {
+    name: "PSYLOCKE",
+    role: "DUELIST"
+  },
+
+  {
+    name: "SCARLET WITCH",
+    role: "DUELIST"
+  },
+
+  {
+    name: "SPIDER-MAN",
+    role: "DUELIST"
+  },
+
+  {
+    name: "SQUIRREL GIRL",
+    role: "DUELIST"
+  },
+
+  {
+    name: "STAR-LORD",
+    role: "DUELIST"
+  },
+
+  {
+    name: "STORM",
+    role: "DUELIST"
+  },
+
+  {
+    name: "THE PUNISHER",
+    role: "DUELIST"
+  },
+
+  {
+    name: "WINTER SOLDIER",
+    role: "DUELIST"
+  },
+
+  {
+    name: "WOLVERINE",
+    role: "DUELIST"
+  },
+
+
+  // ==========================================
   // STRATEGISTS
-  { name: "ADAM WARLOCK", role: "STRATEGIST" },
-  { name: "CLOAK & DAGGER", role: "STRATEGIST" },
-  { name: "INVISIBLE WOMAN", role: "STRATEGIST" },
-  { name: "JEFF THE LAND SHARK", role: "STRATEGIST" },
-  { name: "LOKI", role: "STRATEGIST" },
-  { name: "LUNA SNOW", role: "STRATEGIST" },
-  { name: "MANTIS", role: "STRATEGIST" },
-  { name: "ROCKET RACCOON", role: "STRATEGIST" },
-  { name: "ULTRON", role: "STRATEGIST" },
-  { name: "WHITE FOX", role: "STRATEGIST" },
+  // ==========================================
 
-  // MULTI-ROLE
-  { name: "DEADPOOL", role: "VANGUARD / DUELIST / STRATEGIST" },
+  {
+    name: "ADAM WARLOCK",
+    role: "STRATEGIST"
+  },
 
-  // NEW
-  { name: "GORR", role: "DUELIST" }
+  {
+    name: "CLOAK & DAGGER",
+    role: "STRATEGIST"
+  },
+
+  {
+    name: "GAMBIT",
+    role: "STRATEGIST"
+  },
+
+  {
+    name: "INVISIBLE WOMAN",
+    role: "STRATEGIST"
+  },
+
+  {
+    name: "JEFF THE LAND SHARK",
+    role: "STRATEGIST"
+  },
+
+  {
+    name: "JUBILEE",
+    role: "STRATEGIST"
+  },
+
+  {
+    name: "LOKI",
+    role: "STRATEGIST"
+  },
+
+  {
+    name: "LUNA SNOW",
+    role: "STRATEGIST"
+  },
+
+  {
+    name: "MANTIS",
+    role: "STRATEGIST"
+  },
+
+  {
+    name: "ROCKET RACCOON",
+    role: "STRATEGIST"
+  },
+
+  {
+    name: "ULTRON",
+    role: "STRATEGIST"
+  },
+
+  {
+    name: "WHITE FOX",
+    role: "STRATEGIST"
+  }
 
 ];
